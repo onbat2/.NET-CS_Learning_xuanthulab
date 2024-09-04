a4a3ca8509d4535456d934f5e89d749d99bac316
onbat2 <quangduc11202@gmail.com>
2024-09-04 17:05:01 +0700

    CREATE CS001

---
+++ CS002/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+float a, b;
+string sinput;
+
+Console.WriteLine("Hay nhap so a: ");
+sinput = Console.ReadLine();
+a = float.Parse(sinput);
+
+Console.WriteLine("Hay nhap so b: ");
+sinput = Console.ReadLine();
+b = Convert.ToSingle(sinput);
+
+Console.WriteLine("So a = {0}, so b = {1}", a, b);
