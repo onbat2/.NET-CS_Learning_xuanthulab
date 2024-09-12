fd105a3a8b62839e8bf7c3b3ba396378d41e8c0b
onbat2 <quangduc11202@gmail.com>
2024-09-12 16:48:18 +0700

    CREATE CS006

---
+++ xuanthulab/CS006/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+int a, b;
+
+Console.WriteLine("Nhap so a: ");
+a = int.Parse(Console.ReadLine());
+Console.WriteLine("Nhap so b: ");
+b = int.Parse(Console.ReadLine());
+
+L1:
+Console.WriteLine("Hay chon lenh: ");
+Console.WriteLine("1, Tinh tong");
+Console.WriteLine("2, Tinh hieu");
+Console.WriteLine("3, Tinh tich");
+Console.WriteLine("4, Tinh thuong");
+
+char c;
+c = Console.ReadKey().KeyChar;
+Console.WriteLine();
+
+switch (c) 
+{
+    case '1':
+        Console.WriteLine($"Tong la: {a + b}");
+    break;
+    case '2':
+        Console.WriteLine($"Hieu la: {a - b}");
+    break;
+    case '3':
+        Console.WriteLine($"Tich la: {a * b}");
+    break;
+    case '4':
+        Console.WriteLine($"Thuong la: {a / b}");
+    break;
+    default:
+        Console.WriteLine("!!! WARNING: Hay nhap lai lenh");
+    goto L1;
+}
