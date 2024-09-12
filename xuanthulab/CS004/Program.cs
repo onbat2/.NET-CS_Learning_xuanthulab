7ae40bcd0221480b9b7b7ea50a06db2b511baec3
onbat2 <quangduc11202@gmail.com>
2024-09-12 14:54:48 +0700

    CREATE CS004

---
+++ xuanthulab/CS004/Program.cs
@@ -0,0 +1,27 @@
+﻿// toan tu so sanh
+// == > < >= <= !=
+
+bool result;
+
+int a = 5; 
+int b = 6;
+result = a == b;
+
+Console.WriteLine("1");
+Console.WriteLine("a == b ===> {0}", result);
+Console.WriteLine($"a != b ===> {a != b}");
+Console.WriteLine("");
+
+// toan tu logic
+// && || !
+
+bool m = false;
+bool n = true;
+bool Im_True = true;
+
+bool result1 = m && n;
+
+Console.WriteLine("2");
+Console.WriteLine("m && n ===> {0}", result1);
+Console.WriteLine($"m || n ===> {m || n}");
+Console.WriteLine($"!Im_True ===> {!Im_True}");
